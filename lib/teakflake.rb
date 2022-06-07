f46395c71747d12dc307f3bfc51199c995f27871
Alex Scarborough <alex@teak.io>
2022-06-07 10:29:39 -0700

    Initial commit.

---
+++ lib/teakflake.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "teakflake/version"
+
+module Teakflake
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/teakflake/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Teakflake
+  VERSION = "0.1.0"
+end
